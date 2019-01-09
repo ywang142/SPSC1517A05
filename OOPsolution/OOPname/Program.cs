4b5e79d8f27c65c374cc19a80648a3978c33a1b7
ywang142 <46543398+ywang142@users.noreply.github.com>
2019-01-09 16:48:08 -0700

    create die class

--- OOPsolution/OOPname/Program.cs
+++ OOPsolution/OOPname/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace OOPname
 {
+    // Data member
+
+    // Properties - the interface of the instance  for a data member to a user
+
+    // Characteristics - size - int
+    //                 - color - string
+    //                 - face - int
+
+    // Constructor - use to set up an initial state of your object (instance)
+
+    // Method - actions available to the user
+
+    // Roll() - randomly create a value between min max
+    //        - assign to the face property
+
+    // Unstand the problem ----- Design solution ----- codes
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
---
+++ OOPsolution/OOPname/die.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPname
+{
+    public class die
+    {
+        // this is the definition of a object
+        // it is a conceptual view of the data 
+        //   that will be held by a physical 
+        //   instance (object) of this class
+
+        // Data Members
+        // is an internal private data storage item
+        // private data members CANNOT be reached directly by the user
+        // public data members CAN be reached directly by the user
+        private int _Size;
+
+        // Properties
+        // a property is an external interface between the user and a single piece of data within the instance
+        // a property has two abilities
+        //      a) the ability to assign a value to the internal data member
+        //      b) return a internal data member value to the user
+
+        // Fully Implemented Property
+        public int Size
+        {
+            // takes internal values and returns it to the user
+            get { return _Size; }
+            set
+            {
+                // takes the supplied user value and places it into the internal private data member
+                // the incoming piece of data is placed into a special variable that is called: value
+                _Size = value;
+            }
+        }
+        // Constructors
+
+        // Behaviours (methods)
+    }
+}
